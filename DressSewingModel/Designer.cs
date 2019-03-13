66fb6609f0aee190212b99c5f0beae3485dc9e50
Илья Котов <zwaren@yandex.ru>
2019-03-13 15:06:52 +0300

    Intermediate

--- DressSewingModel/Designer.cs
+++ DressSewingModel/Designer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,11 @@ namespace DressSewingModel
     public class Designer
     {
         public int Id { get; set; }
+
+        [Required]
         public string DesignerFIO { get; set; }
+
+        [ForeignKey("DesignerId")]
+        public virtual List<Request> Requests { get; set; }
     }
 }
