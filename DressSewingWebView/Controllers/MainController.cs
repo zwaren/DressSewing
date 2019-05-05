4db42048071136f3620e26363e574253a283d05b
zwaren <zwaren@yandex.ru>
2019-05-06 00:42:26 +0400

    Implement one for other entities

---
+++ DressSewingWebView/Controllers/MainController.cs
@@ -0,0 +1,15 @@
+﻿using DressSewingServiceDAL.BindingModels;
+using DressSewingServiceDAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DressSewingWebView.Controllers
+{
+    public class MainController : Controller
+    {
+        
+    }
+}
